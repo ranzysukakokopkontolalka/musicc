e797e08ece69913c0950c971d2fa268640156d62
Not faund <flaelva6@gmail.com>
2025-06-18 20:06:12 +0700

    Update ping.py

--- ChampuMusic/plugins/tools/ping.py
+++ ChampuMusic/plugins/tools/ping.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from pyrogram import filters
 from pyrogram.types import Message
 
-from config import BANNED_USERS, PING_VIDEO_URL
+from config import BANNED_USERS, PING_IMG_URL
 from ChampuMusic import app
 from ChampuMusic.core.call import Champu
 from ChampuMusic.utils import bot_sys_stats
@@ -14,14 +14,14 @@ from ChampuMusic.utils.inline import support_group_markup
 @app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
-    response = await message.reply_video(
-        photo=PING_VIDEO_URL,
+    response = await message.reply_photo(
+        photo=PING_IMG_URL,
         caption=_["ping_1"].format(app.mention),
     )
     start = datetime.now()
     pytgping = await Champu.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
-    resp = (datetime.now() - start).microseconds / 10000000
+    resp = (datetime.now() - start).microseconds / 100000000000000
     await response.edit_text(
         _["ping_2"].format(
             resp,
