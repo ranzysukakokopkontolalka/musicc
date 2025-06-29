a403115559b4895abc3305552821d254d1d36560
ranzysukakokopkontolalka <ipanalkenzi@gmail.com>
2025-06-29 22:01:22 +0700

    Update start.py

--- ChampuMusic/plugins/bot/start.py
+++ ChampuMusic/plugins/bot/start.py
@@ -59,7 +59,6 @@ async def ban_new(client, message):
 async def start_comm(client, message: Message, _):
     chat_id = message.chat.id
     await add_served_user(message.from_user.id)
-    await message.react("😈")
     if len(message.text.split()) > 1:
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
